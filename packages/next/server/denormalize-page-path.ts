6b224afef41097e7cf7128d61395cc517b607f11
Javi Velasco <javier.velasco86@gmail.com>
2021-06-17 16:36:12 +0200

    Add more comments

--- packages/next/server/denormalize-page-path.ts
+++ packages/next/server/denormalize-page-path.ts
@@ -1,7 +1,22 @@
+/**
+ * Removes all escape characters from a given path for each of the separators.
+ * Allows to chance from /foo\/bar\/baz to /foo/bar/baz.
+ *
+ * @param path The path to normalized.
+ * @returns The normalized path.
+ */
 export function normalizePathSep(path: string): string {
   return path.replace(/\\/g, '/')
 }
 
+/**
+ * Removes all escape characters from a given page path and removes the
+ * leading /index if it exists. When the page is /index it normalizes to
+ * simply the root /.
+ *
+ * @param page Page to dernomalize.
+ * @returns The denormalized page.
+ */
 export function denormalizePagePath(page: string) {
   page = normalizePathSep(page)
   if (page.startsWith('/index/')) {
