8957c63d48d79a4cef4b1095302556cc3426b3b5
Javi Velasco <javier.velasco86@gmail.com>
2021-06-25 09:00:31 +0200

    getDataHref -> getInternalHref

--- packages/next/client/page-loader.ts
+++ packages/next/client/page-loader.ts
@@ -86,12 +86,19 @@ export default class PageLoader {
    * @param {string} asPath the URL as shown in browser (virtual path); used for dynamic routes
    * @returns {string}
    */
-  getDataHref(
-    href: string,
-    asPath: string,
-    ssg: boolean,
+  getInternalHref({
+    href,
+    asPath,
+    ssg,
+    segment = 'data',
+    locale,
+  }: {
+    href: string
+    asPath: string
+    ssg: boolean
     locale?: string | false
-  ): string {
+    segment: 'edge' | 'data'
+  }): string {
     const { pathname: hrefPathname, query, search } = parseRelativeUrl(href)
     const { pathname: asPathname } = parseRelativeUrl(asPath)
     const route = normalizeRoute(hrefPathname)
@@ -102,7 +109,7 @@ export default class PageLoader {
         '.json'
       )
       return addBasePath(
-        `/_next/data/${this.buildId}${dataRoute}${ssg ? '' : search}`
+        `/_next/${segment}/${this.buildId}${dataRoute}${ssg ? '' : search}`
       )
     }
 
