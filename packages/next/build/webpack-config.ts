de6cc448f6fc0f40f5183f3353b0525de1c54ab5
Javi Velasco <javier.velasco86@gmail.com>
2021-07-01 21:41:49 +0200

    Add next-edge-loader

--- packages/next/build/webpack-config.ts
+++ packages/next/build/webpack-config.ts
@@ -1048,6 +1048,7 @@ export default async function getBaseWebpackConfig(
         'next-serverless-loader',
         'noop-loader',
         'next-style-loader',
+        'next-edge-loader',
       ].reduce((alias, loader) => {
         // using multiple aliases to replace `resolveLoader.modules`
         alias[loader] = path.join(__dirname, 'webpack', 'loaders', loader)
