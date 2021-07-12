e8f6ecfc2609a4d6111fd228c304593b001f2943
Javi Velasco <javier.velasco86@gmail.com>
2021-07-12 19:43:48 +0200

    Add nonDynamicRoutes to routes manifest

--- packages/next/build/index.ts
+++ packages/next/build/index.ts
@@ -95,6 +95,7 @@ import { normalizeLocalePath } from '../shared/lib/i18n/normalize-locale-path'
 import { isWebpack5 } from 'next/dist/compiled/webpack/webpack'
 
 const staticCheckWorker = require.resolve('./utils')
+const RESERVED_PAGE_REGEX = /(^\/(_app|_error|_document|api(\/|$))|_edge$)/
 
 export type SsgRoute = {
   initialRevalidateSeconds: number | false
@@ -384,6 +385,12 @@ export default async function build(
         namedRegex?: string
         routeKeys?: { [key: string]: string }
       }>
+      nonDynamicRoutes: Array<{
+        page: string
+        regex: string
+        namedRegex?: string
+        routeKeys?: { [key: string]: string }
+      }>
       dataRoutes: Array<{
         page: string
         routeKeys?: { [key: string]: string }
@@ -409,6 +416,19 @@ export default async function build(
       headers: headers.map((r: any) => buildCustomRoute(r, 'header')),
       dynamicRoutes: getSortedRoutes(pageKeys)
         .filter(isDynamicRoute)
+        .map((page) => {
+          const routeRegex = getRouteRegex(page)
+          return {
+            page,
+            regex: normalizeRouteRegex(routeRegex.re.source),
+            routeKeys: routeRegex.routeKeys,
+            namedRegex: routeRegex.namedRegex,
+          }
+        }),
+      nonDynamicRoutes: getSortedRoutes(pageKeys)
+        .filter(
+          (page) => !isDynamicRoute(page) && !page.match(RESERVED_PAGE_REGEX)
+        )
         .map((page) => {
           const routeRegex = getRouteRegex(page)
           return {
@@ -763,11 +783,7 @@ export default async function build(
             let isHybridAmp = false
             let ssgPageRoutes: string[] | null = null
 
-            const nonReservedPage = !page.match(
-              /(^\/(_app|_error|_document|api(\/|$))|_edge$)/
-            )
-
-            if (nonReservedPage) {
+            if (!page.match(RESERVED_PAGE_REGEX)) {
               try {
                 let isPageStaticSpan = checkPageSpan.traceChild(
                   'is-page-static'
